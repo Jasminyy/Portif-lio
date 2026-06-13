9b62bab631340ac3a3cbff937601363ab27fd8ac
Jasminy156 <jasminy_santos@estudante.sesisenai.org.br>
2026-06-13 17:12:08 -0300

    Adicionando detalhes

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,13 +1,13 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Technologies from "../components/Technologies";
-import Projects from "../components/Projects";
-import Education from "../components/Education";
-import Stats from "../components/Stats";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
-import MouseGlow from "../components/MouseGlow";
+import Navbar from "../components/Navbar.jsx";
+import Hero from "../components/Hero.jsx";
+import About from "../components/About.jsx";
+import Technologies from "../components/Technologies.jsx";
+import Projects from "../components/Projects.jsx";
+import Education from "../components/Education.jsx";
+import Stats from "../components/Stats.jsx";
+import Contact from "../components/contact.jsx";
+import Footer from "../components/Footer.jsx";
+import MouseGlow from "../components/MouseGLow.jsx";
 
 export default function Home() {
   return (
